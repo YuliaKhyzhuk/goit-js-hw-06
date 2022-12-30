7db411b03a3934c95021ed0780ad2a0e6f48f345
YuliaKhyzhuk <anastezy1@gmail.com>
2022-12-30 22:44:49 +0200

    Виконано завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientList = document.querySelector('#ingredients');
+
+const createIngredient = ingredient => {
+  const ingredientItemEl = document.createElement('li');
+  ingredientItemEl.classList.add('item')
+
+  // firstItem.textContent = "Ajax";
+  ingredientItemEl.textContent = ingredient;
+
+  return ingredientItemEl;
+}
+
+const recipeIngredients = ingredients.map(ingredient => createIngredient(ingredient));
+
+ingredientList.append(...recipeIngredients);
