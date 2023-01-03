6dfcee58f5b290f6a8ea281ba1777723c107b0b5
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-03 23:45:31 +0200

    Виконано завдання 4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+let outputEl = document.querySelector("#value");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+const onDecrementBtnClick = (event) => {
+  counterValue -= 1;
+  outputEl.textContent = counterValue;
+};
+
+const onIncrementBtnClick = (event) => {
+  counterValue += 1;
+  outputEl.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", onDecrementBtnClick);
+incrementBtn.addEventListener("click", onIncrementBtnClick);
