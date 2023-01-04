c91c9fe95ad526512646bb36aeef152eb160b68b
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-04 06:03:00 +0200

    Виконане завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const changeColorBtnEl = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+const onChangeColorBtnElClick = (event) => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  console.log(randomColor);
+  colorSpan.textContent = randomColor;
+  console.log(colorSpan);
+};
+
+changeColorBtnEl.addEventListener("click", onChangeColorBtnElClick);
