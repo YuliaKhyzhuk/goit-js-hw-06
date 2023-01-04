045ad674c0ff10d9d0a2faac9105294f68de7e6a
YuliaKhyzhuk <anastezy1@gmail.com>
2023-01-04 05:41:52 +0200

    Виконано завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const contactFormEl = document.querySelector(".login-form");
+const onContactFormSubmit = (event) => {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+    return;
+  }
+
+  const userDataObject = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userDataObject);
+
+  event.currentTarget.reset();
+};
+
+contactFormEl.addEventListener("submit", onContactFormSubmit);
